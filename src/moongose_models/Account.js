2d3416a2ff5f5c7bfb3a4658f5dc5262ab3987d9
Chris Porell <cporell@yahoo.com>
2023-01-01 21:48:48 -0500

    first commit

---
+++ src/moongose_models/Account.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Account = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Account', Account);
---
+++ src/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { PORT, DATABASE_URL } = process.env;
+const mongoose = require('mongoose');
+mongoose.connect(DATABASE_URL);
+
+const upload = require('multer')();
+
+const path = require('node:path');
+const fs = require('node:fs');
+
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+for (const page of fs.readdirSync(path.join(__dirname, 'pages/'))) {
+    app.use(`/${page}`, express.static(path.join(__dirname, `pages/${page}`)));
+}
+
+app.post('/login/', upload.none(), async (req, res) => {
+    const AccountModel = require('./moongose_models/Account');
+
+    const account = await AccountModel.findOne({ username: req.body.username});
+    if (account) {
+        if (account.password = req.body.password) {
+            res.redirect('/success');
+        }
+        else {
+
+        }
+    }
+    else {
+
+    }
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('ERROR: Page not found');
+});
+
+app.listen(PORT);
